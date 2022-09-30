eac324bc697b81b007acdd314731f73c70b553de
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2022-09-30 07:49:36 -0500

    fix: latest properties

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,44 @@
 import { Container } from "@mui/system";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import CardList from "../components/card/CardList";
 import PropertyCard from "../components/card/PropertyCard";
-import {exampleProps} from "../components/card/PropertyCard";
+import { exampleProps } from "../components/card/PropertyCard";
 
 export function Home() {
+    const [properties, setProperties] = useState([]);
+    const fetchProperties = async () => {
+        const result = await axios.get(`http://localhost:8080/api/properties`);
+        setProperties(result.data.content);
+    }
+    useEffect(() => {
+        fetchProperties();
+    }, [])
     return (
         <div>
             <div className="home">
-                <h1>Home</h1>
+                <h1>Property Portal</h1>
             </div>
             <Container>
                 <CardList>
-                    <PropertyCard {...{...exampleProps, id: 1}}/>
-                    <PropertyCard {...{...exampleProps, id: 2}}/>
-                    <PropertyCard {...{...exampleProps, id: 3}}/>
-                    <PropertyCard {...{...exampleProps, id: 4}}/>
-                    <PropertyCard {...{...exampleProps, id: 5}}/>
-                    <PropertyCard {...{...exampleProps, id: 6}}/>
+                    {
+                        properties.map((property) => {
+                            return (
+                                <PropertyCard {...{
+                                    ...exampleProps,
+                                    id: property.id,
+                                    price: property.price,
+                                    type: property.type,
+                                    listingType: property.listingType,
+                                    numberOfRooms: property.numberOfRooms,
+                                    address: property.address
+                                }} />
+                            )
+                        })
+                    }
                 </CardList>
             </Container>
-                
+
         </div>
     );
 }
